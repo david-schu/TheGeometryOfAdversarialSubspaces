49e5e0efd9e58256514f46aa63addfb95c95498a
david-schu <67379862+david-schu@users.noreply.github.com>
2020-12-01 18:07:14 +0100

    override carlini wagner attack, added plot functions

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+
+
 def orth_check(adv_dirs):
     orth = np.eye(len(adv_dirs))
     for i in range(len(adv_dirs)):
@@ -7,14 +11,16 @@ def orth_check(adv_dirs):
 
     return orth
 
+
 def classification(img, model):
     pred = model(img).detach().numpy()
-    img_class = np.argmax(pred,axis=-1)
+    img_class = np.argmax(pred, axis=-1)
     return img_class
+
 
 def dirs_to_attack_format(dirs):
     max_dim = max(len(elem) for elem in dirs)
-    attack_dirs=np.zeros([len(dirs),max_dim,dirs[0].shape[-1]])
-    for i,dir in enumerate(dirs):
-        attack_dirs[i,:len(dir)] = dir
+    attack_dirs = np.zeros([len(dirs), max_dim, dirs[0].shape[-1]])
+    for i, dir in enumerate(dirs):
+        attack_dirs[i, :len(dir)] = dir
     return torch.tensor(attack_dirs)
