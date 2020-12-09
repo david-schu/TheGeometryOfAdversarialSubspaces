4a0279f59d638c0b5605451edf85201b267049c9
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2020-12-09 17:04:15 +0100

    Update decomp_orth_const.py

--- decomp_orth_const.py
+++ decomp_orth_const.py
@@ -25,19 +25,19 @@ fmodel = foolbox.models.PyTorchModel(model,
                                      bounds=(0., 1.),
                                      device=u.dev())
 
-n_images = 5
+n_images = 1
 images, labels = load_data(n_images, bounds=(0., 1.))
 
 # user initialization
 attack_params = {
         'binary_search_steps': 12,
         'initial_const': 1e-2,
-        'steps': 500,
+        'steps': 10000,
         'confidence': 1,
         'abort_early': True
     }
 params = {
-    'n_adv_dims': 2,
+    'n_adv_dims': 10,
     'max_runs': 3,
     'early_stop': 3,
     'input_attack': CarliniWagner,
