92448630ca66d46d3e516d704d7c4339dc65d79c
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2020-12-09 13:09:35 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -24,9 +24,9 @@ def classification(img, model):
 def dirs_to_attack_format(dirs):
     max_dim = max(len(elem) for elem in dirs)
     attack_dirs = np.zeros([len(dirs), max_dim, dirs[0].shape[-1]])
-    for i, dir in enumerate(dirs):
-        attack_dirs[i, :len(dir)] = dir
-    return torch.tensor(attack_dirs,device=u.dev())
+    for i, d in enumerate(dirs):
+        attack_dirs[i, :len(d)] = d
+    return torch.tensor(attack_dirs, device=u.dev())
 
 
 def load_data(n, bounds=(0., 1.)):
@@ -36,7 +36,7 @@ def load_data(n, bounds=(0., 1.)):
     images = images.unsqueeze(1)
     labels = mnist.targets[:n]
 
-    images = torch.as_tensor(images,device=u.dev())
+    images = torch.as_tensor(images, device=u.dev())
     labels = torch.as_tensor(labels, device=u.dev())
     return images, labels
 
