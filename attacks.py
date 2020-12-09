8aeeae0bf561d2c29aded22a58e179d8cb6aa22d
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2020-12-09 17:01:35 +0100

    Update attacks.py

--- attacks.py
+++ attacks.py
@@ -200,9 +200,8 @@ class CarliniWagner(fa.L2CarliniWagnerAttack):
                 np.isinf(upper_bounds), consts_exponential_search, consts_binary_search
             )
         if plot_loss and len(dirs):
-            print(sys.path)
             fig, ax = plot_losses(losses[best_binary_search_step])
             plt.suptitle('Loss functions for orth_const = ' + str(orth_const))
-            plt.savefig('./data/figures/losses/losses' + str(orth_const) + '.png')
+            plt.savefig('./data/figures/losses' + str(orth_const) + '.png')
 
         return restore_type(best_advs)
