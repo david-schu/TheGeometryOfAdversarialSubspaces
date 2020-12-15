e1a0f28acb8fdb3d3fd0d283e2207b470f39d73e
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2020-12-15 15:49:31 +0100

    Update eval.py

--- models/eval.py
+++ models/eval.py
@@ -39,7 +39,7 @@ def evalAdvAttack(fgsm_model=None, test_loader=None):
         fmodel = foolbox.models.PyTorchModel(fgsm_model,  # return logits in shape (bs, n_classes)
                                              bounds=(0., 1.),  # num_classes=10,
                                              device=u.dev())
-        attack = fa.LinfProjectedGradientDescentAttack(rel_stepsize=0.01 / 0.3,
+        attack = fa.LinfProjectedGradientDescentAttack(abs_stepsize=0.01,
                                                        steps=100,
                                                        random_start=True, )
         xs, _, success = attack(fmodel, xs, ys, epsilons=[0.3])
