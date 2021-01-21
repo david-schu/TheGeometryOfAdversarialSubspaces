faf60602b10de3c0f7ef51b763d8bd3d68f8a720
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-01-21 21:33:20 +0100

    Update train_madry.py

--- train_madry.py
+++ train_madry.py
@@ -13,7 +13,7 @@ max_num_training_steps = 100000
 train_batch_size = 50
 eval_batch_size = 200
 learning_rate = 1e-4
-epsilon=[0.5]
+epsilon=[0.7]
 
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=True, download=True,
@@ -27,12 +27,12 @@ test_loader = torch.utils.data.DataLoader(
 
 nb_epochs = 8  #int(max_num_training_steps/len(train_loader)) + 1
 
-print("Training Model")
-model_normal.trainTorch(train_loader, nb_epochs, learning_rate)
-
-# Evaluation
-eval.evalClean(model_normal, test_loader)
-eval.evalAdvAttack(model_normal, test_loader, epsilon=epsilon)
+# print("Training Model")
+# model_normal.trainTorch(train_loader, nb_epochs, learning_rate)
+#
+# # Evaluation
+# eval.evalClean(model_normal, test_loader)
+# eval.evalAdvAttack(model_normal, test_loader, epsilon=epsilon)
 
 print("Training on Adversarial Samples")
 model_adv.advTrain(train_loader, nb_epochs, learning_rate, epsilon=epsilon)
