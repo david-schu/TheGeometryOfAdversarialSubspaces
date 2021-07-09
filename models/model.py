e474c0bbc0c041bb781fb01ebdc4bddd04592d31
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-07-09 13:21:14 +0200

    Update model.py

--- models/model.py
+++ models/model.py
@@ -165,9 +165,7 @@ class cifar_model(torch.nn.Module):
         x = self.maxPool3(x)
         x = x.view(-1, 128 * 4 * 4)
         x = self.relu(self.fc1(x))
-        x = self.dropout(x)
         x = self.fc2(x)
-        x = self.dropout(x)
         return x
 
     def trainTorch(self,
