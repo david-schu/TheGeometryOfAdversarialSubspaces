9eef97dcced432e5a863d108fc75fde0a596d24d
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-07-16 16:50:20 +0200

    added differentiable model

--- models/model.py
+++ models/model.py
@@ -138,6 +138,67 @@ class madry(torch.nn.Module):
                     totalAdv = 0
                     correctAdv = 0
 
+
+class madry_diff(torch.nn.Module):
+    def __init__(self, dirs=[], x0=[]):
+        super(madry_diff, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5, padding=2)
+        self.maxPool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, 5, padding=2)
+        self.maxPool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(64 * 7 * 7, 1024)
+        self.fc2 = nn.Linear(1024, 10)
+        self.elu = nn.ELU()
+        self.dirs = dirs
+        self.x0 = x0
+
+    def forward(self, x):
+        x = x.clip(0, 1)
+        x = self.elu(self.conv1(x))
+        x = self.maxPool1(x)
+        x = self.elu(self.conv2(x))
+        x = self.maxPool2(x)
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.elu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+    def trainTorch(self,
+                   train_loader,
+                   nb_epochs=NB_EPOCHS,
+                   learning_rate=LEARNING_RATE
+                   ):
+        optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        train_loss = []
+        total = 0
+        correct = 0
+        step = 0
+        for _epoch in range(nb_epochs):
+            for xs, ys in train_loader:
+                xs, ys = Variable(xs), Variable(ys)
+                if torch.cuda.is_available():
+                    xs, ys = xs.cuda(), ys.cuda()
+                optimizer.zero_grad()
+                preds = self(xs)
+                loss = nn.CrossEntropyLoss()
+                output = loss(preds, ys)
+                output.backward()  # calc gradients
+                train_loss.append(output.data.item())
+                optimizer.step()  # update gradients
+
+                preds_np = preds.cpu().detach().numpy()
+                correct += (np.argmax(preds_np, axis=1) == ys.cpu().detach().numpy()).sum()
+                total += train_loader.batch_size
+                step += 1
+                if total % 1000 == 0:
+                    acc = float(correct) / total
+                    print('[%s] Training accuracy: %.2f%%' % (step, acc * 100))
+                    total = 0
+                    correct = 0
+
+
+
 class cifar_model(torch.nn.Module):
     def __init__(self):
         super(cifar_model, self).__init__()
