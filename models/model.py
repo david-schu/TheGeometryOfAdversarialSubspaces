986a05421f51c5f61142d60a29e6b8a7d30e35ae
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-07-25 11:16:48 +0200

    chnaged stepsize

--- models/model.py
+++ models/model.py
@@ -90,7 +90,7 @@ class model_trainable(torch.nn.Module):
                                                      bounds=(0., 1.),  # num_classes=10,
                                                      device=u.dev())
                 attack = fa.L2ProjectedGradientDescentAttack(abs_stepsize=0.01,
-                                                               steps=100,
+                                                               steps=500,
                                                                random_start=True)
                 xs, _, success = attack(fmodel, xs, ys, epsilons=epsilon)
 
