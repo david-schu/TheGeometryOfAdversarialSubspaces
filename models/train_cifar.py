1a54c1a19e81c0f502725c9dea7484981bb404b6
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-09-24 17:19:48 +0200

    Update train_cifar.py

--- models/train_cifar.py
+++ models/train_cifar.py
@@ -25,8 +25,7 @@ train_args = Parameters(train_kwargs)
 # Fill whatever parameters are missing from the defaults
 train_args = defaults.check_and_fill_args(train_args,
                         defaults.TRAINING_ARGS, CIFAR)
-train_args = defaults.check_and_fill_args(train_args,
-                        defaults.PGD_ARGS, CIFAR)
+
 
 # Train a model
 train.train_model(train_args, m_nat, (train_loader, val_loader), store=out_store)
