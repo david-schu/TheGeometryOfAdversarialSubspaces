72afc2cb10aecde132db5f78520f59fcf8322f9b
Dylan Paiton <dpaiton@bg-slurmb-login1.novalocal>
2021-10-03 20:32:27 +0200

    minor bugfixes

--- Curvature/cifar_curvature.sh
+++ Curvature/cifar_curvature.sh
@@ -5,14 +5,14 @@
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
 #SBATCH --time=2-00:00            # Runtime in D-HH:MM
 #SBATCH --partition=gpu-2080ti    # Partition to submit to
-#SBATCH --mem=100G              # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=curvature.out  	  # File to which STDOUT will be written
-#SBATCH --error=curvature.err      # File to which STDERR will be written
+#SBATCH --mem=100G                # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=curvature_%j.out    # File to which STDOUT will be written
+#SBATCH --error=curvature_%j.err      # File to which STDERR will be written
 #SBATCH --gres=gpu:5              # Request one GPU
 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=dylan.paiton@uni-tuebingen.de
 
-singularity exec --nv docker://davidschultheiss/ad:latest /opt/conda/bin/python3 subspace_curvature $arg1 $arg2 $arg3
+singularity exec --nv docker://davidschultheiss/ad:latest /opt/conda/bin/python3 subspace_curvature.py $arg1 $arg2 $arg3
 
 echo DONE!
