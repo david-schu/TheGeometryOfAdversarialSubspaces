a9a6ba2280f4c87a83eff8bc27ef7d53e62a3bd1
david-schu <david.luis@web.de>
2021-10-03 20:52:58 +0200

    all changes

--- Decomposition/run_multiple.sh
+++ Decomposition/run_multiple.sh
@@ -2,6 +2,6 @@ for i in `seq 0 1`
 do
     for j in `seq 0 49`
     do
-        arg1=0 arg2=$i arg3=$j sbatch batch_run.sh        
+        arg1=1 arg2=$i arg3=$j sbatch batch_run.sh        
     done
 done
