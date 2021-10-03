1b906c475700784895981ba0efc73034e859ad45
Dylan Paiton <dpaiton@bg-slurmb-login1.novalocal>
2021-10-03 20:38:04 +0200

    updates some sbatch flags

--- Curvature/cifar_curvature.sh
+++ Curvature/cifar_curvature.sh
@@ -3,12 +3,12 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --cpus-per-task=5         # Number of CPU cores per task
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
-#SBATCH --time=2-00:00            # Runtime in D-HH:MM
+#SBATCH --time=0-04:00            # Runtime in D-HH:MM
 #SBATCH --partition=gpu-2080ti    # Partition to submit to
 #SBATCH --mem=100G                # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=curvature_%j.out    # File to which STDOUT will be written
-#SBATCH --error=curvature_%j.err      # File to which STDERR will be written
-#SBATCH --gres=gpu:5              # Request one GPU
+#SBATCH --output=curvature_%j.out # File to which STDOUT will be written
+#SBATCH --error=curvature_%j.err  # File to which STDERR will be written
+#SBATCH --gres=gpu:1              # optionally type and number of gpus
 
 #SBATCH --mail-type=END
 #SBATCH --mail-user=dylan.paiton@uni-tuebingen.de
