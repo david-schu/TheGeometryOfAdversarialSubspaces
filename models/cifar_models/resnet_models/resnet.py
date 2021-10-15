bd545a0192f064aef60d53c7e6f831b26d09afee
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-10-15 15:45:47 +0200

    Update resnet.py

--- models/cifar_models/resnet_models/resnet.py
+++ models/cifar_models/resnet_models/resnet.py
@@ -99,14 +99,14 @@ class ResNet(nn.Module):
             self.in_planes = planes * block.expansion
         return SequentialWithArgs(*layers)
 
-    def forward(self, x, with_latent=False, fake_rsilu=False, no_rsilu=False):
-        assert (not no_rsilu),  \
-            "no_rsilu not yet supported for this architecture"
+    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+        assert (not no_relu),  \
+            "no_relu not yet supported for this architecture"
         out = F.silu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out, fake_rsilu=fake_rsilu)
+        out = self.layer4(out, fake_relu=fake_relu)
         out = F.avg_pool2d(out, 4)
         pre_out = out.view(out.size(0), -1)
         final = self.linear(pre_out)
