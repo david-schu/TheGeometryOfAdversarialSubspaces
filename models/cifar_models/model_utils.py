560c0bfaf6a074047a2b7cff2086e20e8b15fb7a
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-10-18 11:22:18 +0200

    Update model_utils.py

--- models/cifar_models/model_utils.py
+++ models/cifar_models/model_utils.py
@@ -8,7 +8,7 @@ from models.cifar_models.resnet import CifarPretrained
 
 
 def make_and_restore_model(*_, arch, dataset, resume_path=None,
-                           parallel=False, pytorch_pretrained=False, add_custom_forward=False):
+                           parallel=False, attack_model=False):
     """
     Makes a model and (optionally) restores it from a checkpoint.
 
@@ -37,7 +37,7 @@ def make_and_restore_model(*_, arch, dataset, resume_path=None,
     """
 
     classifier_model = cifar_models.__dict__[arch](num_classes=dataset.num_classes)
-    if resume_path:
+    if attack_model:
         model = CifarPretrained(classifier_model, dataset)
     else:
         model = AttackerModel(classifier_model, dataset)
