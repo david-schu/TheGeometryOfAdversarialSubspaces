d982527793db1cc9c6a5b09f63338dc39d808398
david-schu <david.luis@web.de>
2021-11-11 19:48:36 +0100

    changed script

--- conv.sh
+++ conv.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                      # Number of tasks (see below)
 #SBATCH --cpus-per-task=5               # Number of CPU cores per task
 #SBATCH --nodes=1                       # Ensure that all cores are on one machine
-#SBATCH --time=02:00                  # Runtime in D-HH:MM
+#SBATCH --time=1-00:00                  # Runtime in D-HH:MM
 #SBATCH --partition=gpu-2080ti          # Partition to submit to
 #SBATCH --mem=100G                      # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=run_model_training_%j.out # File to which STDOUT will be written
