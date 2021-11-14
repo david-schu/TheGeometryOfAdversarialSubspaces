9975a502a38dc5b1089bd5d6a21e7ddbd5da3cc3
DavidSchultheiss <67379862+david-schu@users.noreply.github.com>
2021-11-14 18:16:08 +0100

    Update dist_to_dec_bnd.py

--- dist_to_dec_bnd.py
+++ dist_to_dec_bnd.py
@@ -1,50 +1,51 @@
 import numpy as np
 from utils import load_model
-
+import sys
 from utils import get_dist_dec
 
 import tqdm
 
-dset='CIFAR'
-is_natural = 1
+if __name__ == "__main__":
+    is_natural = int(sys.argv[1])
+    dset='CIFAR'
 
-#load model
-if is_natural:
-    model_path = './../models/cifar_models/nat_diff_new.pt'
-    data_path = './data/cifar_runs/cifar_natural_wrn.npy'
-    save_path = './data/dists_to_bnd_natural_wrn.npy'
-else:
-    model_path = './../models/cifar_models/rob_diff_new.pt'
-    data_path = './data/cifar_runs/cifar_robust_wrn.npy'
-    save_path = './data/dists_to_bnd_robust_wrn.npy'
+    #load model
+    if is_natural:
+        model_path = './../models/cifar_models/nat_diff_new.pt'
+        data_path = './data/cifar_runs/cifar_natural_wrn.npy'
+        save_path = './data/dists_to_bnd_natural_wrn.npy'
+    else:
+        model_path = './../models/cifar_models/rob_diff_new.pt'
+        data_path = './data/cifar_runs/cifar_robust_wrn.npy'
+        save_path = './data/dists_to_bnd_robust_wrn.npy'
 
-model = load_model(resume_path=model_path, dataset=dset)
+    model = load_model(resume_path=model_path, dataset=dset)
 
-# load data
-data = np.load(data_path, allow_pickle=True).item()
-pert_lengths = data['pert_lengths']
-dirs = data['dirs']
-images = data['images']
-labels = data['labels']
+    # load data
+    data = np.load(data_path, allow_pickle=True).item()
+    pert_lengths = data['pert_lengths']
+    dirs = data['dirs']
+    images = data['images']
+    labels = data['labels']
 
-n_samples = 100
-n_dims = 50
+    n_samples = 100
+    n_dims = 50
 
-#natural
-min_dists = pert_lengths[:, 0]
+    #natural
+    min_dists = pert_lengths[:, 0]
 
-dists = np.zeros((len(images), n_dims, n_samples))
-angles= np.zeros((len(images), n_dims, n_samples))
-largest_vecs = np.zeros((len(images), n_dims, dirs.shape[-1]))
-for i, img in enumerate(tqdm.tqdm(images)):
-    for n in np.arange(0, n_dims):
-        dists[i, n], angles[i, n], largest_vecs[i, n] = get_dist_dec(img, labels[i], dirs[i, :n + 1], model,
-                                                                     min_dist=0.5 * min_dists[i],
-                                                                     n_samples=n_samples)
+    dists = np.zeros((len(images), n_dims, n_samples))
+    angles= np.zeros((len(images), n_dims, n_samples))
+    largest_vecs = np.zeros((len(images), n_dims, dirs.shape[-1]))
+    for i, img in enumerate(tqdm.tqdm(images)):
+        for n in np.arange(0, n_dims):
+            dists[i, n], angles[i, n], largest_vecs[i, n] = get_dist_dec(img, labels[i], dirs[i, :n + 1], model,
+                                                                         min_dist=0.5 * min_dists[i],
+                                                                         n_samples=n_samples)
 
-    data = {
-        'dists': dists,
-        'angles': angles,
-        'largest_vecs': largest_vecs
-    }
-    np.save(save_path, data)
+        data = {
+            'dists': dists,
+            'angles': angles,
+            'largest_vecs': largest_vecs
+        }
+        np.save(save_path, data)
