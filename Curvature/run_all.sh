059675654251636e2b9f114d03155a57f9d7eeb8
Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>
2021-11-18 15:40:17 +0100

    only start jobs which are not already running
    Signed-off-by: Matthias Kümmmerer <matthias.kuemmerer@bethgelab.org>

--- Curvature/run_all.sh
+++ Curvature/run_all.sh
@@ -2,10 +2,17 @@
 #do
     for j in {0..7}
     do
-        for k in {0..3}
+        for k in {7..9}
         do
             #arg1=$i arg2=$j arg3=$k sbatch --job-name=curvature_analysis-$i-$j-$k curvature_experiments.sh
-            arg1=1 arg2=$j arg3=$k sbatch --job-name=curvature_analysis-1-$j-$k curvature_experiments.sh
+            JOB_NAME=curvature_analysis-1-$j-$k
+            IS_RUNNING=$(squeue --name="$JOB_NAME" --noheader)
+            if [ -z "$IS_RUNNING" ]; then
+                echo "starting $JOB_NAME"
+                arg1=1 arg2=$j arg3=$k sbatch --job-name=$JOB_NAME curvature_experiments.sh
+            else
+                echo "already running $JOB_NAME"
+            fi
         done
     done
 #done
